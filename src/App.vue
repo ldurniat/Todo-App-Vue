9ccdd7b3aabbbe4160e43a8d57a6c03614042119
ldurniat <ldurniat@gmail.com>
2023-08-24 17:38:54 +0200

    Bind newTodo variable with input value

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ function addNewTodo() {
     <h1>Vue 3 Todo  App</h1>
     <form action="" @submit.prevent="addNewTodo">
         <label>New Todo</label>
-        <input type="text" name="newTodo">
+        <input type="text" v-model="newTodo" name="newTodo">
         <button>Add New Todo</button>
     </form>
 </template>
