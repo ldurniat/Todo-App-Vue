c8163c8b98b9a2c2a27e8d93789da12632f31d60
ldurniat <ldurniat@gmail.com>
2023-08-25 18:03:12 +0200

    Add styles to improve apperance

--- src/App.vue
+++ src/App.vue
@@ -53,7 +53,21 @@ function removeAllTodos() {
 </template>
 
 <style scoped>
-
+body {
+  font-family: sans-serif;
+  padding-top: 1em;
+  padding-bottom: 1em;
+  font-size: 2em;
+  width: 80%;
+  margin: 0 auto;
+}
+input, textarea, button, p, div, section, article, select {
+  display: 'block';
+  width: 100%;
+  font-family: sans-serif;
+  font-size: 1em;
+  margin: 0.5em;
+}
 .todo {
     cursor: pointer;
 }
